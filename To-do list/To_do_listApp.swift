b5851d264b28d9af4c1b95812b02c522a6113253
Junaid Muhammad Khan <imjunaid.mkhan@gmail.com>
2025-03-08 06:50:06 -0800

    Initial Commit

---
+++ To-do list/To_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_listApp.swift
+//  To-do list
+//
+//  Created by Junaid Muhammad Khan on 2025-03-08.
+//
+
+import SwiftUI
+
+@main
+struct To_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
