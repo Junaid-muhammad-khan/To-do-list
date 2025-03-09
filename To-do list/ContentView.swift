cdaac7a0e63b2fc08327fda72ec1f541754680ff
Junaid Muhammad Khan <imjunaid.mkhan@gmail.com>
2025-03-08 21:05:54 -0800

    Ui Layout changes

--- To-do list/ContentView.swift
+++ To-do list/ContentView.swift
@@ -10,14 +10,79 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                topBar()
+                list()
         }
-        .padding()
     }
 }
+struct list: View {
+    @State private var tasks: [String] = []
+    @State private var newTask = ""
+    var body: some View {
+        VStack {
+                    TextField("Enter a task", text: $newTask)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    
+                    List {
+                        ForEach(tasks, id: \.self) { task in
+                            Text(task)
+                        }
+                        .onDelete { indexSet in
+                            tasks.remove(atOffsets: indexSet)
+                        }
+                    }
+                }
+    }
+}
+struct topBar: View {
+    var body: some View {
+        HStack{
+            Text("To-do list")
+                .bold()
+            Spacer()
+            ellipsis()
+            
+        }
+    }
+}
+struct ellipsis: View {
+    @State private var showMenu = false
+    var body: some View {
+        Button(action: {
+            showMenu = true
+        }){
+            Label("", systemImage: "ellipsis")
+                .foregroundStyle(.black)
+                .ignoresSafeArea()
+                .cornerRadius(10)
+                
+            }
+        .sheet(isPresented: $showMenu, content: {
+            DropdownMenu()
+        })
+            
+        }
+    }
+
+struct DropdownMenu: View {
+    var body: some View {
+        VStack{
+            List{
+                Button(action: {
+                    // tap code
+                }){
+                    Label("Add", systemImage: "plus")
+                        .foregroundStyle(.black)
+                        .ignoresSafeArea()
+                        .cornerRadius(10)
+                        
+                    }
+                EditButton()
+                }
+            }
+        }
+    }
 
 #Preview {
     ContentView()
